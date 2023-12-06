8f79104e2d63249b1d13c78de76a87c875fca3db
YuntianCheng <yuntianchengbest@outlook.com>
2023-12-06 16:40:24 +0800

    fix stop can not stop reading loop

--- multicast_reciever.go
+++ multicast_reciever.go
@@ -2,36 +2,40 @@ package ha
 
 import (
 	"context"
+	"errors"
 	"github.com/sirupsen/logrus"
 	"net"
+	"time"
 )
 
 const MAXREADSIZE = 100
 
 type MulticastHandler func(size int, addr *net.UDPAddr, data []byte)
 
-func (m *manager) listenMultiCast(ctx context.Context, handler MulticastHandler) (err error) {
+func (m *manager) readMultiCastData(ctx context.Context, handler MulticastHandler) (err error) {
 	err = m.conn.SetReadBuffer(MAXREADSIZE)
 	if err != nil {
 		return
 	}
 	defer func() {
-		logrus.Info("quit listening")
+		logrus.Info("quit reading")
 	}()
-	go func() {
-		defer func() {
-			_ = m.conn.Close()
-		}()
-		for {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
 			b := make([]byte, MAXREADSIZE)
-			num, addr, err := m.conn.ReadFromUDP(b)
+			err = m.conn.SetReadDeadline(time.Now().Add(100 * time.Millisecond))
 			if err != nil {
+				return
+			}
+			num, addr, err := m.conn.ReadFromUDP(b)
+			if err != nil && !errors.Is(err, context.DeadlineExceeded) {
 				logrus.Error(err.Error())
 				continue
 			}
 			go handler(num, addr, b)
 		}
-	}()
-	<-ctx.Done()
-	return
+	}
 }
